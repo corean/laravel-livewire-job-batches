7ac9995a772c8d7ddc3a3abc54e6c094ad25a5ad
corean <corean@corean.biz>
2021-09-22 20:38:13 +0900

    - livewire upload 구현

--- app/Http/Livewire/ManageTransfers.php
+++ app/Http/Livewire/ManageTransfers.php
@@ -3,9 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ManageTransfers extends Component
 {
+    use WithFileUploads;
+
+    public $pendingFiles = [];
+
+    public function initiateTransfer()
+    {
+        $this->validate([
+            'pendingFiles.*' => ['image', 'max:5120'],
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.manage-transfers');
--- resources/views/livewire/manage-transfers.blade.php
+++ resources/views/livewire/manage-transfers.blade.php
@@ -1,52 +1,52 @@
-<div>
-  <table>
-    <thead>
+<div class="container mx-auto grid grid-cols-4 gap-4">
+  <table class="col-span-3 h-auto  shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+    <thead class="bg-gray-50">
     <tr>
-      <th>&nbsp;</th>
-      <th>Status</th>
-      <th>Batch ID</th>
-      <th>Storage</th>
+      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">&nbsp;</th>
+      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Batch ID
+      </th>
+      <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Storage
+      </th>
     </tr>
     </thead>
-    <tbody>
+    <tbody class="bg-white divide-y divide-gray-200">
     <tr>
-      <td>√</td>
-      <td>Uploaded</td>
-      <td>d9cbb5a7-ea12-42b4-9fb3-3e5a7f10631f</td>
-      <td>2MB</td>
+      <td class="px-6 py-4 whitespace-nowrap">√</td>
+      <td class="px-6 py-4 whitespace-nowrap">Uploaded</td>
+      <td class="px-6 py-4 whitespace-nowrap">d9cbb5a7-ea12-42b4-9fb3-3e5a7f10631f</td>
+      <td class="px-6 py-4 whitespace-nowrap">2MB</td>
     </tr>
     <tr>
-      <td>X</td>
-      <td>Finished with errors</td>
-      <td>0d669854-fb2c-480f-ae04-8572ec695242</td>
-      <td>0MB</td>
+      <td class="px-6 py-4 whitespace-nowrap">X</td>
+      <td class="px-6 py-4 whitespace-nowrap">Finished with errors</td>
+      <td class="px-6 py-4 whitespace-nowrap">0d669854-fb2c-480f-ae04-8572ec695242</td>
+      <td class="px-6 py-4 whitespace-nowrap">0MB</td>
     </tr>
     <tr>
-      <td>!!</td>
-      <td>Failed</td>
-      <td>e176a925-8534-446f-a1f6-3fc2e06fcb0f</td>
-      <td>0MB</td>
+      <td class="px-6 py-4 whitespace-nowrap">!!</td>
+      <td class="px-6 py-4 whitespace-nowrap">Failed</td>
+      <td class="px-6 py-4 whitespace-nowrap">e176a925-8534-446f-a1f6-3fc2e06fcb0f</td>
+      <td class="px-6 py-4 whitespace-nowrap">0MB</td>
     </tr>
     <tr>
-      <td>
-        <svg
-            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle
-              stroke-width="4"></circle>
+      <td class="px-6 py-4 whitespace-nowrap">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle stroke-width="4"></circle>
           <path
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
       </td>
-      <td>
+      <td class="px-6 py-4 whitespace-nowrap">
         <div class="flex h-2 overflow-hidden rounded bg-gray-50">
           <div style="transform: scale({{ 50 / 100 }}, 1)"
                class="bg-indigo-500 transition-transform origin-left duration-200 ease-in-out w-full shadow-none flex flex-col"></div>
         </div>
       </td>
-      <td>
+      <td class="px-6 py-4 whitespace-nowrap">
         296fc64e-af31-401d-9895-3d18ce02931c
       </td>
-      <td>
+      <td class="px-6 py-4 whitespace-nowrap">
         0MB
       </td>
     </tr>
@@ -54,31 +54,41 @@
   </table>
 
 
-  <div>
-    <h3>Create Batch</h3>
+  <div class=" shadow border-b border-gray-200 sm:rounded-lg bg-white p-2 ml-2">
+    <h3 class="text-lg font-bold">Create Batch</h3>
     <p>Select the files you want to upload.</p>
 
-    <div>
-      <input id="files" name="files" type="file" multiple>
+    <div class="form-control">
+      <input id="files" wire:model="pendingFiles"  name="files" type="file" multiple wire class="border w-full">
     </div>
 
-    <div>
+    <div class="bor">
       Files
     </div>
 
 
     <div>
-      <img src="#" alt="">
-      <img src="#" alt="">
-      <img src="#" alt="">
+      <div>
+        @forelse($pendingFiles as $pendingFile)
+          <img src="{{ $pendingFile->temporaryUrl() }}"
+               alt="">
+        @empty
+          <p>No files selected</p>
+        @endforelse
+      </div>
     </div>
 
     <div>
-      <button type="button">
+      @error('pendingFiles.*')
+      {{ $message }}
+      @enderror
+    </div>
+
+    <div>
+      <button type="button" wire:click="initiateTransfer">
         Do some magic
 
-        <svg
-            xmlns="http://www.w3.org/2000/svg">
+        <svg xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
         </svg>
